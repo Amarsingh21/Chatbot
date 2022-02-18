5fd483e23e3869a8222ff793f9ad9fc8e59e97fc
Amar <53298944+Amarsingh21@users.noreply.github.com>
2022-02-18 23:43:13 +0530

    Update ChatBox.js

--- src/Components/chatbot1/ChatBox.js
+++ src/Components/chatbot1/ChatBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './css/chatbox.css';
 import Request from './Request';
-
+import {Link} from 'react-router-dom'
 
 function ChatBox() {
   return (
       <div className="chatbox">
         <div className="chatboxTitle">
           <div className="TitleText">Support is Online</div>
+          <Link to ="/">
+          <div className='TitleText closeBtn'>X</div>
+          </Link>
         </div>
       <div className='messageBox'>
         <Request />
